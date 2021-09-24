9b10b8163b8cbb9ffbbccf96d95a2d37866324cf
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2021-09-24 19:33:01 +0700

    add product api
    in > src/data.json

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ newbie
 
 stater from :
 https://www.youtube.com/watch?v=hQAu0YEIF0g
+
+next step:
+https://www.youtube.com/watch?v=72DYDMP09MM
--- express-netlify/src/api.js
+++ express-netlify/src/api.js
@@ -3,12 +3,16 @@ const serverless = require('serverless-http');
 
 const app = express();
 const router = express.Router();
+
+const importData = require("./data.json")
  
 router.get('/', (req, res) => {
-    res.json({
-        'hello': 'hi'
-    });
+    res.send("api");
 });
+
+router.get('/product', (req, res) => {
+    res.send(importData);
+})
 
 router.get('/test', (req, res) => {
     res.json({
