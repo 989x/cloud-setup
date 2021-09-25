f03c0315cea31b6156360f5625f160bab7a9b849
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2021-09-26 00:12:58 +0700

    add > images in data.json
    and edit > api.js

--- express-netlify/src/api.js
+++ express-netlify/src/api.js
@@ -5,7 +5,14 @@ const app = express();
 const router = express.Router();
 
 const importData = require("./data.json")
- 
+
+
+//new
+const path = require('path'); 
+
+const fs = require('fs');
+
+
 router.get('/', (req, res) => {
     res.send("api");
 });
@@ -20,6 +27,20 @@ router.get('/test', (req, res) => {
     });
 });
 
+
+router.get('/images', (req,res) => {
+    // res.send(fs.readFileSync('./images/1.png'))
+    fs.readFile(`./images/${req.query.image}`, 'base64', (er, image) => {
+        const data = (`data:image/png;base64,${image}`);
+        res.send(`<img src=${data} />`)
+    })
+})
+
+
 app.use('/.netlify/functions/api',router);
+
+//new
+// router.use(express.static('dist'))
+// router.use('/images', express.static(path.join(__dirname, 'images')))
 
 module.exports.handler = serverless(app);
