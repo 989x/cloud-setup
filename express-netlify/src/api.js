d26d863ef7852a1c460a06ce23b4b38450f61cf8
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2021-09-29 16:35:04 +0700

    add > /product/:id
    for > /.netlify/functions/api/product/1

--- express-netlify/src/api.js
+++ express-netlify/src/api.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
 
-const importData = require("./data.json")
-
+const importData = require("./data.json");
 
 //new
-const path = require('path'); 
+const path = require("path");
 
-const fs = require('fs');
+const fs = require("fs");
 
-
-router.get('/', (req, res) => {
-    res.send("api");
+router.get("/", (req, res) => {
+  res.send("api");
 });
 
-router.get('/product', (req, res) => {
-    res.send(importData);
-})
-
-router.get('/test', (req, res) => {
-    res.json({
-        'hello': 'test'
-    });
+router.get("/product", (req, res) => {
+  res.send(importData);
 });
 
+//
+router.get("/product/:id", (req, res) => {
 
-router.get('/images', (req,res) => {
-    // res.send(fs.readFileSync('./images/1.png'))
-    fs.readFile(`./images/${req.query.image}`, 'base64', (er, image) => {
-        const data = (`data:image/png;base64,${image}`);
-        res.send(`<img src=${data} />`)
-    })
-})
+  res.send(importData.results.find((r) => r.id == req.params.id));
+});
 
+router.get("/test", (req, res) => {
+  res.json({
+    hello: "test",
+  });
+});
 
-app.use('/.netlify/functions/api',router);
+router.get("/images", (req, res) => {
+  // res.send(fs.readFileSync('./images/1.png'))
+  fs.readFile(`./images/${req.query.image}`, "base64", (er, image) => {
+    const data = `data:image/png;base64,${image}`;
+    res.send(`<img src=${data} />`);
+  });
+});
+
+app.use("/.netlify/functions/api", router);
 
 //new
 // router.use(express.static('dist'))
